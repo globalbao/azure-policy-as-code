05b293bbeb6e78f86f0553e543d3b8ac9a96cf83
Jesse Loudon <gitbao@outlook.com>
2021-08-19 17:50:37 +1000

    fix casing

--- Terraform/modules/policyset-definitions/main.tf
+++ Terraform/modules/policyset-definitions/main.tf
@@ -159,7 +159,7 @@ METADATA
     reference_id         = "(linuxSecurityLogs)Configure Association to link Linux virtual machines to Data Collection Rule"
     parameter_values = jsonencode(
       {
-        "DcrResourceId" : { value = "/subscriptions/42482d91-3f4f-4012-8e45-78bf7ad4d60c/resourceGroups/DataCollectionRules/providers/Microsoft.Insights/dataCollectionRules/linuxSecurityLogs" }
+        "dcrResourceId" : { value = "/subscriptions/42482d91-3f4f-4012-8e45-78bf7ad4d60c/resourceGroups/DataCollectionRules/providers/Microsoft.Insights/dataCollectionRules/linuxSecurityLogs" }
       }
     )
   }
@@ -169,7 +169,7 @@ METADATA
     reference_id         = "(linuxPerformanceLogs)Configure Association to link Linux virtual machines to Data Collection Rule"
     parameter_values = jsonencode(
       {
-        "DcrResourceId" : { value = "/subscriptions/42482d91-3f4f-4012-8e45-78bf7ad4d60c/resourceGroups/DataCollectionRules/providers/Microsoft.Insights/dataCollectionRules/linuxPerformanceLogs" }
+        "dcrResourceId" : { value = "/subscriptions/42482d91-3f4f-4012-8e45-78bf7ad4d60c/resourceGroups/DataCollectionRules/providers/Microsoft.Insights/dataCollectionRules/linuxPerformanceLogs" }
       }
     )
   }
@@ -184,7 +184,7 @@ METADATA
     reference_id         = "(windowsSecurityLogs)Configure Association to link Windows virtual machines to Data Collection Rule"
     parameter_values = jsonencode(
       {
-        "DcrResourceId" : { value = "/subscriptions/42482d91-3f4f-4012-8e45-78bf7ad4d60c/resourceGroups/DataCollectionRules/providers/Microsoft.Insights/dataCollectionRules/windowsSecurityLogs" }
+        "dcrResourceId" : { value = "/subscriptions/42482d91-3f4f-4012-8e45-78bf7ad4d60c/resourceGroups/DataCollectionRules/providers/Microsoft.Insights/dataCollectionRules/windowsSecurityLogs" }
       }
     )
   }
@@ -194,7 +194,7 @@ METADATA
     reference_id         = "(windowsPerformanceLogs)Configure Association to link Windows virtual machines to Data Collection Rule"
     parameter_values = jsonencode(
       {
-        "DcrResourceId" : { value = "/subscriptions/42482d91-3f4f-4012-8e45-78bf7ad4d60c/resourceGroups/DataCollectionRules/providers/Microsoft.Insights/dataCollectionRules/windowsPerformanceLogs" }
+        "dcrResourceId" : { value = "/subscriptions/42482d91-3f4f-4012-8e45-78bf7ad4d60c/resourceGroups/DataCollectionRules/providers/Microsoft.Insights/dataCollectionRules/windowsPerformanceLogs" }
       }
     )
   }
